7b7cb63156f2b2fdc1656053a2f45e6a0d81887c
javiducun <javiducun@gmail.com>
2023-05-23 17:26:49 +0200

    second test

--- test/userLogin.test.ts
+++ test/userLogin.test.ts
@@ -8,4 +8,16 @@ describe('User Service Login', () => {
     const myUser = new User('javiducun')
     expect(service.manualLogin(myUser)).toEqual('User successfully logged in')
   })
+
+  it('is user already loged in', () => {
+    // arrange
+    const myUser = new User('javiducun2')
+
+    // act
+    service.manualLogin(myUser)
+    const response = service.manualLogin(myUser)
+
+    // asert
+    expect(response).toEqual('User already logged in')
+  })
 })
